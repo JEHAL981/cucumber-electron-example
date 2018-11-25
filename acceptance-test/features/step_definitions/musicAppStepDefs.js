cfdc017a512a568fff0d735bd82d9178320afecc
James Halls <JEHalls81@gmail.com>
2018-11-25 16:59:24 +0000

    Update npm libaries cucumber and electron

--- acceptance-test/features/step_definitions/musicAppStepDefs.js
+++ acceptance-test/features/step_definitions/musicAppStepDefs.js
@@ -1,19 +1,19 @@
-const cucumber = require('cucumber');
-const hyperdom = require('hyperdom');
-const vineHill = require('vinehill');
-const browserMonkey = require('browser-monkey');
-const Client = require('../../../app/music');
-const server = require('../../../app/server');
+import cucumber from 'cucumber';
+import { Given, When, Then } from "cucumber";
+import { append } from 'hyperdom';
+import vineHill from 'vinehill';
+import { component } from 'browser-monkey';
+import Client from '../../../app/music';
+import server from '../../../app/server';
 
 
 
 const MusicAppUrl = "http://localhost:5000";
 
-cucumber.defineSupportCode(function ({Given, When, Then}) {
     Given('I am using the music app', function () {
         vineHill({[MusicAppUrl]: server});
-        hyperdom.append(document.body, new Client(MusicAppUrl));
-        this.monkey = browserMonkey.component(document.body)
+        append(document.body, new Client(MusicAppUrl));
+        this.monkey = component(document.body)
     });
 
     When('I open the Music Genre Rock', function () {
@@ -24,7 +24,5 @@ cucumber.defineSupportCode(function ({Given, When, Then}) {
         return this.monkey.shouldFind('h1', {text: 'MUSE'});
     });
 
-
-});
 
 
